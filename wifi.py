ebe9da867ab429e62d10e4d641a30c9d6feb1b2c
Kesara Rathnayake <kesara@fq.nz>
2024-08-25 00:36:52 +1200

    feat: Add wifi.py

---
+++ wifi.py
@@ -0,0 +1,38 @@
+from machine import Pin
+from utime import sleep_ms
+
+import network
+import urequests
+
+LED = Pin("LED", Pin.OUT)
+DELAY = 1000
+SSID = "rp2-pico"
+PASSWORD = "kiwipycon"
+
+
+def connect():
+    connection = network.WLAN(network.STA_IF)
+    connection.active(True)
+    connection.ifconfig(
+        [
+            "192.168.2.147",
+            "255.255.255.0",
+            "192.168.2.1",
+            "9.9.9.9",
+        ]
+    )
+    connection.connect(SSID, PASSWORD)
+    while connection.status() != network.STAT_GOT_IP:
+        # blink LED while connecting
+        LED.value(1)
+        sleep_ms(DELAY)
+        LED.value(0)
+        sleep_ms(DELAY)
+
+
+connect()
+
+response = urequests.get("http://ip.jsontest.com/")
+print(response.text)
+response = urequests.get("http://date.jsontest.com/")
+print(response.text)
